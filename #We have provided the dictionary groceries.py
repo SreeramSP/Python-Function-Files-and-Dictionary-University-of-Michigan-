b826c96c5697ae546ab9e752f57a94afa94c68af
Sreeram SP <sreeramsp1234@gmail.com>
2021-09-13 23:55:24 +0530

    Add files via upload

---
+++ #We have provided the dictionary groceries.py
@@ -0,0 +1,7 @@
+#We have provided the dictionary groceries. You should return a list of its keys, but they should be sorted by their values, from highest to lowest. Save the new list as most_needed.
+
+
+groceries = {'apples': 5, 'pasta': 3, 'carrots': 12, 'orange juice': 2, 'bananas': 8, 'popcorn': 1, 'salsa': 3, 'cereal': 4, 'coffee': 5, 'granola bars': 15, 'onions': 7, 'rice': 1, 'peanut butter': 2, 'spinach': 9}
+def most(key,d):
+    return d[key]
+most_needed = sorted(groceries.keys(), reverse = True, key = lambda x : most(x, groceries))
