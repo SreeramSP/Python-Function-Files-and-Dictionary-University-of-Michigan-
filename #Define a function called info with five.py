165c7a2668932c171681149cf828282f488e16b7
Sreeram SP <sreeramsp1234@gmail.com>
2021-09-13 23:50:38 +0530

    Add files via upload

---
+++ #Define a function called info with five.py
@@ -0,0 +1,6 @@
+#Define a function called info with five parameters: name, gender, age, bday_month, and hometown. The function should then return a tuple with all five parameters in that order
+
+def info(name, gender,age,bday_month,hometown):
+    details=(name,gender,age,bday_month,hometown)
+    return(details)
+info('anitha','Female',25,12,'Chennai')
