8beeaf7b0e265a1f47e0cf8ed8f2b3cc0cd17d19
Sreeram SP <sreeramsp1234@gmail.com>
2021-09-13 23:58:21 +0530

    Add files via upload

---
+++ #You will need to write two functions for.py
@@ -0,0 +1,7 @@
+#You will need to write two functions for this problem. The first function, divide that takes in any number and returns that same number divided by 2. The second function called sum should take any number, divide it by 2, and add 6. It should return this new number. You should call the divide function within the sum function. Do not worry about decimals.
+
+def divide(i):
+    return i/2
+def sum(i):
+    return i/2+6
+sum(divide(20))
