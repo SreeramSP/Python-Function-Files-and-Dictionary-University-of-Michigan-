1240bdf2cdab3ecd5f5a6e2ee85fd73f91a7b304
Sreeram SP <sreeramsp1234@gmail.com>
2021-09-13 23:56:05 +0530

    Add files via upload

---
+++ #Write a function called change that take.py
@@ -0,0 +1,5 @@
+#Write a function called change that takes any string, adds “Nice to meet you!” to the end of the argument given, and returns that new string.
+
+def change(str):
+    return str+'Nice to meet you!'
+print(change('Welcome'))
